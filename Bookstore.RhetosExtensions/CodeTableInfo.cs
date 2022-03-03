bb0c85b324a3760160362e827fea33bff4026b75
Pavao Križić <pkrizic@omega-software.hr>
2022-03-03 16:16:58 +0100

    try Extending DSL - Develop a macro concept
    not implement demonstrate and assignment tasks

---
+++ Bookstore.RhetosExtensions/CodeTableInfo.cs
@@ -0,0 +1,17 @@
+﻿using Rhetos.Dsl;
+using Rhetos.Dsl.DefaultConcepts;
+using System.ComponentModel.Composition;
+
+namespace Bookstore.RhetosExtensions
+{
+    [Export(typeof(IConceptInfo))]
+    [ConceptKeyword("CodeTable")]
+    public class CodeTableInfo : IConceptInfo
+    {
+        [ConceptKey]
+        public ShortStringPropertyInfo Code { get; set; }
+
+        [ConceptKey]
+        public RequiredPropertyInfo Name { get; set; }
+    }
+}
---
+++ Bookstore.RhetosExtensions/CodeTableMacro.cs
@@ -0,0 +1,18 @@
+﻿using Rhetos.Dsl;
+using Rhetos.Dsl.DefaultConcepts;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+
+namespace Bookstore.RhetosExtensions
+{
+    [Export(typeof(IConceptMacro))]
+    public class CodeTableMacro : IConceptMacro<CodeTableInfo>
+    {
+        public IEnumerable<IConceptInfo> CreateNewConcepts(CodeTableInfo conceptInfo, IDslModel existingConcepts)
+        {
+            var newConcepts = new List<IConceptInfo>();
+
+            return newConcepts;
+        }
+    }
+}
